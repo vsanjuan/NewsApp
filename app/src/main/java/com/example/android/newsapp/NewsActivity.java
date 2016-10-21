6e081cebea5e291412e19878dc9f22de5c5db8f6
salvador.sanjuan@gmail.com <salvador.sanjuan@gmail.com>
2016-10-21 06:10:54 +0200

    Feat: News Adapter and parsing working

--- app/src/main/java/com/example/android/newsapp/NewsActivity.java
+++ app/src/main/java/com/example/android/newsapp/NewsActivity.java
@@ -8,8 +8,11 @@ import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -17,7 +20,8 @@ public class NewsActivity extends AppCompatActivity
         implements LoaderManager.LoaderCallbacks<List<News>>  {
 
     private static final String GUARDIAN_API_URL =
-            "http://content.guardianapis.com/search?q=debates&show-tags=contributor&api-key=2ff892df-c635-4cb4-89a8-e64aa6046043content.guardianapis.com/search?q=debates&show-tags=contributor&api-key=2ff892df-c635-4cb4-89a8-e64aa6046043";
+            "http://content.guardianapis.com/search?q=debates&show-tags=contributor" +
+                    "&api-key=2ff892df-c635-4cb4-89a8-e64aa6046043";
 
     private static final String LOG_TAG = NewsActivity.class.getName();
 
@@ -30,13 +34,30 @@ public class NewsActivity extends AppCompatActivity
     /** TextView that is displayed when the list is empty */
     private TextView mEmptyStateTextView;
 
+    /** Adapter for the list of earthquakes */
+    private NewsAdapter mAdapter;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
 
+        // Find a reference to the {@link ListView} in the layout
+        ListView newsListView = (ListView) findViewById(R.id.list);
+
+
         mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
+        newsListView.setEmptyView(mEmptyStateTextView);
+
+        // Create a new adapter taht takes an empty list of earthquakes as input
+        mAdapter = new NewsAdapter(this, new ArrayList<News>());
+
+        // Set the adapter on the {@link ListView}
+        // so the list can be populated in the user interface
+        newsListView.setAdapter(mAdapter);
+
 
         // Get a reference to the ConnectivityManager to check state of network connectivity
         ConnectivityManager connMgr = (ConnectivityManager)
@@ -48,8 +69,6 @@ public class NewsActivity extends AppCompatActivity
         // If there is a network connection, fetch data
         if (networkInfo != null && networkInfo.isConnected()) {
             // Get a reference to the LoaderManager, in order to interact with loaders.
-
-            //TODO: Implement Loader Manager
             LoaderManager loaderManager = getLoaderManager();
 
             // Initialize the loader. Pass in the int ID constant defined above and pass in null for
@@ -79,10 +98,29 @@ public class NewsActivity extends AppCompatActivity
 
 
     @Override
-    public void onLoadFinished(Loader<List<News>> loader, List<News> earthquakes) {
+    public void onLoadFinished(Loader<List<News>> loader, List<News> news) {
 
-        mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
-        mEmptyStateTextView.setText("Loaded");
+        // Hide loading indicator because the data has been loaded
+        View loadingIndicator = findViewById(R.id.progress_bar);
+        loadingIndicator.setVisibility(View.GONE);
+
+        // Set empty state text to display "No earthquakes found."
+        if (news.size()==0){
+
+            mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
+            mEmptyStateTextView.setText(R.string.no_news);
+
+
+        } else {
+            // Hide the
+            mEmptyStateTextView.setVisibility(View.GONE);
+            mAdapter.addAll(news);
+
+        }
+
+
+
+
 
 
     }
---
+++ app/src/main/java/com/example/android/newsapp/NewsAdapter.java
@@ -0,0 +1,62 @@
+package com.example.android.newsapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Salvador on 20/10/2016.
+ */
+
+public class NewsAdapter extends ArrayAdapter<News> {
+
+    /**
+     * Constructs a new {@link NewsAdapter}.
+     *
+     * @param context of the app
+     * @param news  is the list of news, which is the data source of the adapter
+     */
+
+    public NewsAdapter(Context context, List<News> news) {
+
+        super(context,0,news);
+
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        // Check if there is an existing list item view (called convertView) that we can reuse,
+        // otherwise, if convertView is null, then inflate a new list item layout.
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.news_list, parent, false);
+        }
+
+        // Find the news at the given position in the list of earthquakes
+        News currentNews = getItem(position);
+
+        // Find the TextView with the section
+        TextView sectionView = (TextView) listItemView.findViewById(R.id.section);
+        // Display the section of the current news in that TextView
+        sectionView.setText(currentNews.getmSection());
+
+        TextView authorView = (TextView) listItemView.findViewById(R.id.author);
+        // TODO: Format the view and populate it with an author's Array
+        authorView.setText(currentNews.getmAuthor().get(0));
+
+        TextView titleView = (TextView) listItemView.findViewById(R.id.title);
+        titleView.setText(currentNews.getmTitle());
+
+        return listItemView;
+
+    }
+
+
+}
